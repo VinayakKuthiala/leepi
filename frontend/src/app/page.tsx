c4313070ae0f2383c247ce00359ff5ae1683cc15
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2025-07-19 01:35:03 +0530

    Fixing small ui issue in homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       {/* Product Categories Section */}
       <div className="my-12">
         <div className="text-center mb-8">
-          <h2 className="text-3xl xl:text-4xl font-bold text-gray-300 mb-4">
+          <h2 className="text-3xl xl:text-4xl font-bold text-gray-600 mb-4">
             Our Service Categories
           </h2>
           <p className="text-gray-600 max-w-2xl lg:text-lg xl:text-xl  mx-auto">
@@ -31,7 +31,7 @@ export default function Home() {
         <CategoryCarousel />
       </div>
 
-      <FeatureProduct />
+      {/* <FeatureProduct /> */}
 
       {/* What You Get Section & Sign Up Button (Elegant UI) */}
       <WhatYouGetSection />
--- frontend/src/components/FeatureProduct/index.tsx
+++ frontend/src/components/FeatureProduct/index.tsx
@@ -45,6 +45,36 @@ const FeatureProduct = ({ isTop }: { isTop?: boolean }) => {
           </div>
         ))}
       </div>
+        <div
+        className={` w-full mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 py-12`}
+      >
+        {products.map((product, idx) => (
+          <div
+            key={product.name}
+            className={`flex flex-col justify-center bg-white rounded-lg sm:rounded-2xl shadow-lg p-3 sm:p-5 lg:p-8 relative ${
+              idx % 2 === 1 ? "md:top-8 md:ml-8" : "z-10"
+            }`}
+          >
+            <div
+              className="w-full h-56 relative mb-6 rounded-lg  sm:rounded-xl overflow-hidden"
+              style={{ minHeight: 224 }}
+            >
+              <Image
+                src={product.img}
+                alt={product.name}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-xl"
+                sizes="(max-width: 768px) 100vw, 50vw"
+              />
+            </div>
+            <h2 className="text-2xl font-bold mb-2 text-gray-900">
+              {product.name}
+            </h2>
+            <p className="text-gray-600 text-base">{product.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
